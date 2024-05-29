234e4ab7e56fb514734def92711df4315869241f
4Lkiny <wendyescarza20007>
2024-05-29 08:42:02 -0300

    teste

---
+++ calculadora.py
@@ -0,0 +1,29 @@
+def calcular(x,y, op=1):
+
+    if(op == 1):
+        print("A soma é: ",x + y)
+    elif(op == 2):    
+        print("O resto é: ",x - y)
+    elif(op == 3):       
+        print("O produto é: ",x * y)
+    elif(op == 4):        
+        print("O quociente é: ",x / y)
+    elif(op == 5):        
+        print("O resto da divisão é: ",x % y)
+    elif(op == 6):        
+        print("A potência é: ",x ** y)
+    
+x = int(input("dígite um número: "))
+y = int(input("Dígite outro número: "))
+
+print("OPERAÇÕES")
+print("1 Adição")
+print("2 Subtraçaõ")
+print("3 Multiplicaçaõ")
+print("4 Divisão")
+print("5 Resto da divisão")
+print("6 Potência")
+
+op = int(input("Escolha a operação: "))
+
+calcular(x,y,op)
---
+++ dicionario2.py
@@ -0,0 +1,14 @@
+pais = {"nome":"Brasil", 'capital':"Brasilia"}
+
+print(pais)
+
+print(pais["nome"])
+print(pais["capital"])
+
+#pais.clear()
+
+print(pais)
+
+#Método get()
+print(pais.get("nome"))
+print(pais.get("qualquer", "Não existe"))
